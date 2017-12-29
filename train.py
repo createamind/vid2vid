133385da6be1fd1d04349bbf4aefcb69d483bbee
zdx <179363811@qq.com>
2017-12-29 19:53:06 +0800

    ...

--- train.py
+++ train.py
@@ -137,6 +137,7 @@ print(range(opt.epoch_count, opt.niter + opt.niter_decay + 1))
 
 print('='*20 + 'Pre-train Generator' + '='*20)
 for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
+#for epoch in range(1):
     epoch_start_time = time.time()
     epoch_iter = 0
 
@@ -213,7 +214,7 @@ for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
         epoch_iter += opt.batchSize
         model.set_input(data)
         g_mse_loss = model.optimize_parameters()
-
+        '''
         if total_steps % opt.display_freq == 0:
             save_result = total_steps % opt.update_html_freq == 0
 
@@ -224,13 +225,14 @@ for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
             # print(visuals)
             print('process video... %s,progress %d' % (vid_path, i) )
             save_videos(web_dir, visuals, vid_path, epoch)
-
+        '''
         if total_steps % opt.print_freq == 0:
             errors = model.get_current_errors()
             t = (time.time() - iter_start_time) / opt.batchSize
             print("epoch: {}, iter: {}, G-loss: {}, time: {} seconds/batch".format(epoch,
                 i, g_mse_loss.data[0], (time.time() - iter_start_time) / opt.batchSize))
             print(model.get_current_errors())
+            print("target seq:\n {} \ngenerated seq: {}".format(model.input_seq, model.gen_seq))
             #visualizer.print_current_errors(epoch, epoch_iter, errors, t)
             #if opt.display_id > 0:
                 #visualizer.plot_current_errors(epoch, float(epoch_iter)/dataset_size, opt, errors)

7819b95995c3514551771416e351a08f856a2674
SRTjiawei <jeven.z@aliyun.com>
2017-12-31 18:22:03 +0800

    generate vid and seq at the same time, updated vid2seq model

--- models/vid2seq_model.py
+++ models/vid2seq_model.py
@@ -6,11 +6,13 @@ from models.base_model import BaseModel
 import models.networks as networks
 import warnings
 
+
 class Vid2SeqModel(BaseModel):
     """
     Generate sequence conditioned on input video data, using GAN with CNN-LSTM generator and
         multi-layer bidirectional LSTM as discriminator
     """
+
     def name(self):
         return 'Vid2SeqModel'
 
@@ -28,7 +30,8 @@ class Vid2SeqModel(BaseModel):
         self.input_B = self.Tensor(opt.batchSize, opt.output_nc,
                                    opt.depth, opt.fineSize, opt.fineSize)
         self.speedX = self.Tensor(opt.batchSize, opt.depth)
-
+        # self.speedX_A = self.Tensor(opt.batchSize, opt.depth)
+        # self.speedX_B = self.Tensor(opt.batchSize, opt.depth)
 
         # load/define networks
         self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf,  # of gen filters in first conv layer
@@ -36,18 +39,18 @@ class Vid2SeqModel(BaseModel):
         if self.isTrain:
             use_sigmoid = opt.no_lsgan
             self.netD_seq = networks.define_D(opt.input_nc + opt.output_nc, opt.ndf,
-                                          opt.which_model_netD_seq,
-                                          opt.n_layers_D, opt.norm, use_sigmoid, opt.init_type, self.gpu_ids)
+                                              opt.which_model_netD_seq,
+                                              opt.n_layers_D, opt.norm, use_sigmoid, opt.init_type, self.gpu_ids)
 
             self.netD_vid = networks.define_D(opt.input_nc + opt.output_nc, opt.ndf,
-                                          opt.which_model_netD_vid,
-                                          opt.n_layers_D, opt.norm, use_sigmoid, opt.init_type, self.gpu_ids)
-
+                                              opt.which_model_netD_vid,
+                                              opt.n_layers_D, opt.norm, use_sigmoid, opt.init_type, self.gpu_ids)
 
         if not self.isTrain or opt.continue_train:
             self.load_network(self.netG, 'G', opt.which_epoch)
             if self.isTrain:
-                self.load_network(self.netD, 'D', opt.which_epoch)
+                self.load_network(self.netD_vid, 'D', opt.which_epoch)
+                self.load_network(self.netD_seq, 'D', opt.which_epoch)
 
         if self.isTrain:
             # 3D Change
@@ -60,20 +63,25 @@ class Vid2SeqModel(BaseModel):
             self.optimizers = []
             self.optimizer_G = torch.optim.Adam(self.netG.parameters(),
                                                 lr=opt.lr, betas=(opt.beta1, 0.999))
-            self.optimizer_D = torch.optim.Adam(self.netD.parameters(),
-                                                lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_D_vid = torch.optim.Adam(self.netD_vid.parameters(),
+                                                    lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_D_seq = torch.optim.Adam(self.netD_seq.parameters(),
+                                                    lr=opt.lr, betas=(opt.beta1, 0.999))
             self.optimizers.append(self.optimizer_G)
-            self.optimizers.append(self.optimizer_D)
+            self.optimizers.append(self.optimizer_D_vid)
+            self.optimizers.append(self.optimizer_D_seq)
+
             for optimizer in self.optimizers:
                 self.schedulers.append(networks.get_scheduler(optimizer, opt))
 
         print('---------- Networks initialized -------------')
         networks.print_network(self.netG)
         if self.isTrain:
-            networks.print_network(self.netD)
+            networks.print_network(self.netD_vid)
+            networks.print_network(self.netD_seq)
         print('-----------------------------------------------')
 
-    def set_input(self, input, is_numpy=Ture):
+    def set_input(self, inputs, is_numpy=True):
         """
         :param is_numpy: using numpy array or not
         :param inputs: a dict contains two inputs forms with key: 'video' and 'target_seq'
@@ -84,26 +92,30 @@ class Vid2SeqModel(BaseModel):
         # input_B = input['B' if AtoB else 'A']
         AtoB = self.opt.which_direction == 'AtoB'
 
-        input_A = torch.from_numpy(input['A' if AtoB else 'B'])
+        input_A = torch.from_numpy(inputs['A' if AtoB else 'B'])
         # print("======input A SIZE==== {0}".format(input_A.size()))
-        input_B = torch.from_numpy(input['B' if AtoB else 'A'])
-        speedX = torch.from_numpy(input["speedX"])
-        self.input_A.resize_(input_A.size()).copy_(input_A)
-        self.input_B.resize_(input_B.size()).copy_(input_B)
-        self.speedX.resize_(speedX.size()).copy_(speedX)
+        input_B = torch.from_numpy(inputs['B' if AtoB else 'A'])
+        speedX = torch.from_numpy(inputs["speedX"])  # with the length lX = lA + lB
+        # speedX_A = torch.from_numpy(input["speedX"][:self.opt.depth])
+        # speedX_B = torch.from_numpy(input["speedX"][self.opt.depth:])
 
-        self.input_seq = self.speedX
+        # self.input_A.resize_(input_A.size()).copy_(input_A)
+        # self.input_B.resize_(input_B.size()).copy_(input_B)
+        # self.speedX.resize_(speedX.size()).copy_(speedX)
+
+        self.input_A = Variable(input_A)
+        self.input_B = Variable(input_B)
+        self.speedX = Variable(speedX)
 
         # convert to cuda
         if self.gpu_ids and torch.cuda.is_available():
             self.input_A = self.input_A.cuda()
             self.input_B = self.input_B.cuda()
-            self.input_seq = self.input_seq.cuda()
+            self.speedX = self.speedX.cuda()
+            # self.speedX_A = self.speedX_A.cuda()
+            # self.speedX_B = self.speedX_B.cuda()
 
-        self.input_vid = self.input_A
-
-        self.image_paths = input['A_paths' if AtoB else 'B_paths']
-
+        # self.image_paths = inputs['A_paths' if AtoB else 'B_paths']
 
         # # numpy to torch tensor
         # if is_numpy:
@@ -121,32 +133,35 @@ class Vid2SeqModel(BaseModel):
         #     self.input_seq = self.input_seq.cuda()
 
     def forward(self):
-        #self.gen_seq = self.netG(self.input_vid)
-        self.real_A = Variable(self.input_A)
-        self.real_B = Variable(self.input_B)
+        # self.real_A = Variable(self.input_A)
+        # self.real_B = Variable(self.input_B)
+        # self.real_speedX = Variable(self.speedX)
+
+        self.real_A = self.input_A
+        self.real_B = self.input_B
+        self.real_speedX = self.speedX
 
         self.fake_B, self.speedX_pred = self.netG(self.real_A)
-        # print("speedX_pred",self.speedX_pred.size())
-        print("...............................")
-        print(self.speedX)
-        print(self.speedX_pred)
+
+        print("." * 10 + "Compare sequences" + "." * 10)
+        print(self.real_speedX.data)
+        print(self.speedX_pred.data)
+        print("." * 10 + "Compare sequences" + "." * 10)
 
     def backward_D(self):
-
         fake_AB = torch.cat((self.real_A, self.fake_B), 1).data
         fake_AB_ = Variable(fake_AB)
+        fake_cat_seq = torch.cat([self.speedX, self.speedX_pred], 2)
         pred_fake = self.netD_vid(fake_AB_.detach())
-        speed_fake = self.netD_seq(self.speedX_pred.detach())
-        self.loss_D_fake = self.criterionGAN(pred_fake, False)+ \
-                           self.criterionGAN(speed_fake, False) #fake speed
+        speed_fake = self.netD_seq(fake_cat_seq.detach())
+        self.loss_D_fake = self.criterionGAN(pred_fake, False) + self.criterionGAN(speed_fake, False)  # fake speed
 
         # Real
         real_AB = torch.cat((self.real_A, self.real_B), 1)
-        pred_real = self.netD_vid(real_AB)
-        speed_real = self.netD_seq(self.speedX.detach())
-        self.loss_D_real = self.criterionGAN(pred_real, True) + \
-                           self.criterionGAN(speed_real, True)
-
+        real_cat_seq = torch.cat([self.speedX, self.speedX], 2)
+        pred_real_vid = self.netD_vid(real_AB.detach())
+        pred_real_seq = self.netD_seq(real_cat_seq.detach())
+        self.loss_D_real = self.criterionGAN(pred_real_vid, True) + self.criterionGAN(pred_real_seq, True)
 
         # # Fake
         # # stop backprop to the generator by detaching fake seq
@@ -170,24 +185,22 @@ class Vid2SeqModel(BaseModel):
 
     def backward_G(self):
 
-
         # First, G(A) should fake the discriminator
         fake_AB = torch.cat((self.real_A, self.fake_B), 1)
         pred_fake = self.netD_vid(fake_AB)
-        speed_fake = self.netD_seq(self.speedX_pred)
+        fake_cat_seq = torch.cat([self.speedX, self.speedX_pred], 2)
+        speed_fake = self.netD_seq(fake_cat_seq)
         self.loss_G_GAN = self.criterionGAN(pred_fake, True) + \
                           self.criterionGAN(speed_fake, True)
 
         # Second, G(A) = B
         self.loss_G_L1_vid = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_A
-        self.loss_G_L1_seq = self.criterionL1(self.gen_seq, self.input_seq) * 5
-        self.loss_G_L1 = self.loss_G_L1_vid +self.loss_G_L1_seq
-        #action
+        self.loss_G_L1_seq = self.criterionL1(self.speedX_pred, self.speedX) * self.opt.lambda_A
+        self.loss_G_L1 = self.loss_G_L1_vid + self.loss_G_L1_seq
+        # action
         # self.action_loss = self.criterionL2(self.action,self.action_prediction)
-        self.loss_G = self.loss_G_GAN + self.loss_G_L1 #+self.action_loss
+        self.loss_G = self.loss_G_GAN + self.loss_G_L1  # +self.action_loss
         self.loss_G.backward()
-
-
 
         # First, G(A) should fool the discriminator
         # pred_fake = self.netD(self.gen_seq)
@@ -200,51 +213,47 @@ class Vid2SeqModel(BaseModel):
         #
         # self.loss_G.backward()
         # return mse loss, for print
-        return self.netG.batch_mse_loss(self.input_vid, self.input_seq)
-
+        return self.netG.batch_mse_loss(self.input_A, self.speedX)
 
     def pretrain_G_step(self):
-        #print(self.input_vid.size())
-        g_loss = self.netG.batch_mse_loss(self.input_vid, self.input_seq)
-        self.gen_seq = self.netG.gen_seq
+        # print(self.input_vid.size())
+        g_loss = self.netG.batch_mse_loss(self.input_A, self.speedX)
+        self.speedX_pred = self.netG.gen_seq
         self.optimizer_G.zero_grad()
         g_loss.backward()
         self.optimizer_G.step()
         return g_loss
 
-
     def pretrain_D_step(self):
-        label_size = list(self.input_seq.size())
+        label_size = list(self.speedX.size())
         label_size[2] = 1
-        target_real = Variable(torch.ones(label_size).resize_(label_size[0], label_size[1] ))
-        target_fake = Variable(torch.zeros(label_size).resize_(label_size[0], label_size[1] ))
+        target_real = Variable(torch.ones(label_size).resize_(label_size[0], label_size[1]))
+        target_fake = Variable(torch.zeros(label_size).resize_(label_size[0], label_size[1]))
         self.forward()
 
         warnings.warn("Using a target size ({}) that is different to the input size ({}) is deprecated. "
                       "Please ensure they have the same size.".format(self.input_seq.size(), target_real.size()))
 
-
-
-        d_loss = self.netD.batch_bce_loss(self.input_seq.cuda(), target_real.cuda())
-        d_loss += self.netD.batch_bce_loss(self.gen_seq.detach().cuda(), target_fake.cuda())
-        self.optimizer_D.zero_grad()
+        d_loss = self.netD_seq.batch_bce_loss(self.speedX.cuda(), target_real.cuda())
+        d_loss += self.netD_seq.batch_bce_loss(self.speedX_pred.detach().cuda(), target_fake.cuda())
+        self.optimizer_D_seq.zero_grad()
         d_loss.backward()
-        self.optimizer_D.step()
+        self.optimizer_D_seq.step()
         return d_loss
-
 
     def optimize_parameters(self):
         self.forward()
 
-        self.optimizer_D.zero_grad()
+        self.optimizer_D_vid.zero_grad()
+        self.optimizer_D_seq.zero_grad()
         self.backward_D()
-        self.optimizer_D.step()
+        self.optimizer_D_vid.step()
+        self.optimizer_D_seq.step()
 
         self.optimizer_G.zero_grad()
         g_mse_loss = self.backward_G()
         self.optimizer_G.step()
         return g_mse_loss
-
 
     def get_current_errors(self):
         return OrderedDict([('G_GAN', self.loss_G_GAN.data[0]),
@@ -254,9 +263,10 @@ class Vid2SeqModel(BaseModel):
                             ])
 
     def get_current_visuals(self):
-        return OrderedDict([('video', self.input_vid), ('fake_seq', self.gen_seq),
-                            ('real_seq', self.input_seq)])
+        return OrderedDict([('real_vid', self.input_A), ('fake_vid', self.fake_B),
+                            ('real_seq', self.speedX), ('fake_seq', self.speedX_pred)])
 
     def save(self, label):
         self.save_network(self.netG, 'G', label, self.gpu_ids)
-        self.save_network(self.netD, 'D', label, self.gpu_ids)
+        self.save_network(self.netD_vid, 'D_vid', label, self.gpu_ids)
+        self.save_network(self.netD_seq, 'D_seq', label, self.gpu_ids)
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-import time , os ,cv2
+import time, os, cv2
 from options.train_options import TrainOptions
 from data.data_loader import CreateDataLoader
 from models.models import create_model
@@ -6,7 +6,6 @@ import ntpath
 import numpy as np
 import skvideo.io
 import time
-
 
 output_video = False
 opt = TrainOptions().parse()
@@ -21,36 +20,37 @@ opt.results_dir = './results/'
 total_steps = 0
 web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.which_epoch))
 
-def ck_array(i,o):
-    i_A = np.transpose(127.5*(i['A']+1.)[0],(1,2,3,0))
-    i_B = np.transpose(127.5*(i['B']+1.)[0],(1,2,3,0))
+
+def ck_array(i, o):
+    i_A = np.transpose(127.5 * (i['A'] + 1.)[0], (1, 2, 3, 0))
+    i_B = np.transpose(127.5 * (i['B'] + 1.)[0], (1, 2, 3, 0))
     o_A = o['real_A']
     o_B = o['real_B']
     a_diff = i_A - o_A
     b_diff = i_B - o_B
-    print('diffa',a_diff.max(),a_diff.min(),a_diff.mean())
+    print('diffa', a_diff.max(), a_diff.min(), a_diff.mean())
     print('diffb', b_diff.max(), b_diff.min(), b_diff.mean())
 
 
 def save_videos(web_dir, visuals, vid_path, epoch):
     vid_dir = os.path.join(web_dir, 'videos')
-    #name = ntpath.basename(vid_path).split('.')[0]
+    # name = ntpath.basename(vid_path).split('.')[0]
     # add data generation time as name
     name = time.strftime('%Y%m%d-%H%M%S')
-    #print("vid_dir: {}".format(vid_dir))
-    #print("name: {}".format(name))
+    # print("vid_dir: {}".format(vid_dir))
+    # print("name: {}".format(name))
 
     A = visuals['real_A'][:, :, :, :3]
-    #print("="*20 + str(A.shape))
+    # print("="*20 + str(A.shape))
     last_A = np.tile(A[-1], (A.shape[0], 1, 1, 1))
-    #print("A_last shape: {}".format(A[-1].shape))
-    #print('last_A: {}'.format(last_A.shape))
+    # print("A_last shape: {}".format(A[-1].shape))
+    # print('last_A: {}'.format(last_A.shape))
     B = visuals['real_B'][:, :, :, :3]
-    first_B  = np.tile(B[0], (A.shape[0], 1, 1, 1))
+    first_B = np.tile(B[0], (A.shape[0], 1, 1, 1))
     fake = visuals['fake_B'][:, :, :, :3]
     first_fake = np.tile(fake[0], (A.shape[0], 1, 1, 1))
     black = np.ones_like(A)
-    last_fake= np.tile(fake[-1], (A.shape[0], 1, 1, 1))
+    last_fake = np.tile(fake[-1], (A.shape[0], 1, 1, 1))
     blackforA = np.concatenate((first_B, first_fake), axis=1)
     blackforBC = np.concatenate((last_A, last_fake), axis=1)
 
@@ -59,34 +59,30 @@ def save_videos(web_dir, visuals, vid_path, epoch):
     vid_BC = np.concatenate((B, fake), axis=1)
     vid_BC2 = np.concatenate((blackforBC, vid_BC), axis=2)
     vid_numpy = np.concatenate((vid_A2, vid_BC2), axis=0)
-    #print("output_img_shape: {}".format(vid_numpy.shape))
+    # print("output_img_shape: {}".format(vid_numpy.shape))
 
-    #vid_numpy = np.concatenate((visuals['real_A'], visuals['real_B'], visuals['fake_B']), axis=2)
+    # vid_numpy = np.concatenate((visuals['real_A'], visuals['real_B'], visuals['fake_B']), axis=2)
     # print(vid_numpy.shape)
     save_path = os.path.join(vid_dir, str(epoch)) + '/'
     if not os.path.exists(save_path):
         os.makedirs(save_path)
     save_name = name + '_' + '.mp4'
 
-    skvideo.io.vwrite(save_path+save_name, vid_numpy,
+    skvideo.io.vwrite(save_path + save_name, vid_numpy,
                       inputdict={'-r': '12'},
                       outputdict={'-r': '12'})
     print('save video at ', save_path + save_name)
 
-    if opt.output_nc>3:
-
-
-
+    if opt.output_nc > 3:
         # Depth Video
-        print("visuals",visuals['real_A'].shape)
+        print("visuals", visuals['real_A'].shape)
         dA = visuals['real_B'][:, :, :, 3]
         dB = visuals['real_B'][:, :, :, 4]
         dfake = visuals['fake_B'][:, :, :, 4]
         dfake_ = visuals['fake_B'][:, :, :, 3]
 
-
         print("=" * 20 + str(dA.shape))
-        dlast_A = np.tile(dA[-1], (dA.shape[0], 1, 1)) #last frame
+        dlast_A = np.tile(dA[-1], (dA.shape[0], 1, 1))  # last frame
         dlast_A_ = np.tile(dfake_[-1], (dA.shape[0], 1, 1))  # last frame
         # print("A_last shape: {}".format(A[-1].shape))
         # print('last_A: {}'.format(last_A.shape))
@@ -95,13 +91,13 @@ def save_videos(web_dir, visuals, vid_path, epoch):
 
         dfirst_fake = np.tile(dfake[0], (dA.shape[0], 1, 1))
         dblack = np.ones_like(dlast_A)
-        dblackforA = np.concatenate((dfirst_B, dfirst_fake), axis=1) # first frame
+        dblackforA = np.concatenate((dfirst_B, dfirst_fake), axis=1)  # first frame
         print("=" * 20 + "dlastA" + str(dlast_A.shape))
         print("=" * 20 + "dblack" + str(dblack.shape))
-        dblackforBC = np.concatenate((dlast_A, dlast_A_), axis=1) ##replace dblack with dlast_A_
+        dblackforBC = np.concatenate((dlast_A, dlast_A_), axis=1)  ##replace dblack with dlast_A_
 
-        dvid_A = np.concatenate((dA, dfake_), axis=1) #replace dblack with dfake_
-        dvid_A2 = np.concatenate((dvid_A, dblackforA), axis=2) # time within A
+        dvid_A = np.concatenate((dA, dfake_), axis=1)  # replace dblack with dfake_
+        dvid_A2 = np.concatenate((dvid_A, dblackforA), axis=2)  # time within A
         dvid_BC = np.concatenate((dB, dfake), axis=1)
         dvid_BC2 = np.concatenate((dblackforBC, dvid_BC), axis=2)
         dvid_numpy = np.concatenate((dvid_A2, dvid_BC2), axis=0)
@@ -120,77 +116,75 @@ def save_videos(web_dir, visuals, vid_path, epoch):
     while output_video:
 
         for i in range(vid_numpy.shape[0]):
-            save_name = name + '_' + str(i) +'.png'
-            #save_name =  str(i) + '.png'
+            save_name = name + '_' + str(i) + '.png'
+            # save_name =  str(i) + '.png'
 
             img = vid_numpy[i][:, :, ::-1]
-            #print(img.shape)
-            print('save path ',save_path+save_name)
-            cv2.imwrite(save_path+save_name, img)
-
+            # print(img.shape)
+            print('save path ', save_path + save_name)
+            cv2.imwrite(save_path + save_name, img)
 
 
 # pretrain generator
 
-print(range(opt.epoch_count, opt.niter + opt.niter_decay + 1))
+# print(range(opt.epoch_count, opt.niter + opt.niter_decay + 1))
+#
+# print('=' * 20 + 'Pre-train Generator' + '=' * 20)
+# for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
+#     # for epoch in range(1):
+#     epoch_start_time = time.time()
+#     epoch_iter = 0
+#
+#     for i, data in enumerate(dataset):
+#
+#         # for key, value in data.items():
+#         #     print(value.shape)
+#
+#         iter_start_time = time.time()
+#         # visualizer.reset()
+#         total_steps += opt.batchSize
+#         epoch_iter += opt.batchSize
+#         model.set_input(data)
+#         g_loss = model.pretrain_G_step()
+#         if total_steps % opt.print_freq == 0:
+#             print("epoch: {}, iter: {}, loss: {}, time: {} seconds/batch".format(epoch,
+#                                                                                  i, g_loss.data[0], (
+#                                                                                          time.time() - iter_start_time) / opt.batchSize))
+#             print("target seq:\n {} \ngenerated seq: {}".format(model.input_seq, model.gen_seq))
+#         if total_steps % opt.save_latest_freq == 0:
+#             print('saving the latest model (epoch %d, total_steps %d)' %
+#                   (epoch, total_steps))
+#             model.save('latest')
+#
+# total_steps = 0
+#
+# print(range(opt.epoch_count, opt.niter + opt.niter_decay + 1))
+#
+# # pre-train discriminator
+# print('=' * 20 + 'Pre-train Discriminator' + '=' * 20)
+# for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
+#     epoch_start_time = time.time()
+#     epoch_iter = 0
+#
+#     for i, data in enumerate(dataset):
+#         iter_start_time = time.time()
+#         # visualizer.reset()
+#         total_steps += opt.batchSize
+#         epoch_iter += opt.batchSize
+#         model.set_input(data)
+#         d_loss = model.pretrain_D_step()
+#         if total_steps % opt.print_freq == 0:
+#             print("epoch: {}, iter: {}, loss: {}, time: {} seconds/batch".format(epoch,
+#                                                                                  i, d_loss.data[0], (
+#                                                                                          time.time() - iter_start_time) / opt.batchSize))
+#         if total_steps % opt.save_latest_freq == 0:
+#             print('saving the latest model (epoch %d, total_steps %d)' %
+#                   (epoch, total_steps))
+#             model.save('latest')
 
-
-print('='*20 + 'Pre-train Generator' + '='*20)
-for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
-#for epoch in range(1):
-    epoch_start_time = time.time()
-    epoch_iter = 0
-
-    for i, data in enumerate(dataset):
-
-        # for key, value in data.items():
-        #     print(value.shape)
-
-        iter_start_time = time.time()
-        #visualizer.reset()
-        total_steps += opt.batchSize
-        epoch_iter += opt.batchSize
-        model.set_input(data)
-        g_loss = model.pretrain_G_step()
-        if total_steps % opt.print_freq == 0:
-            print("epoch: {}, iter: {}, loss: {}, time: {} seconds/batch".format(epoch,
-                i, g_loss.data[0], (time.time() - iter_start_time) / opt.batchSize))
-            print("target seq:\n {} \ngenerated seq: {}".format(model.input_seq, model.gen_seq))
-        if total_steps % opt.save_latest_freq == 0:
-            print('saving the latest model (epoch %d, total_steps %d)' %
-                  (epoch, total_steps))
-            model.save('latest')
-
-
-total_steps = 0
-
-print(range(opt.epoch_count, opt.niter + opt.niter_decay + 1))
-
-# pre-train discriminator
-print('='*20 + 'Pre-train Discriminator' + '='*20)
-for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
-    epoch_start_time = time.time()
-    epoch_iter = 0
-
-    for i, data in enumerate(dataset):
-        iter_start_time = time.time()
-        #visualizer.reset()
-        total_steps += opt.batchSize
-        epoch_iter += opt.batchSize
-        model.set_input(data)
-        d_loss = model.pretrain_D_step()
-        if total_steps % opt.print_freq == 0:
-            print("epoch: {}, iter: {}, loss: {}, time: {} seconds/batch".format(epoch,
-                i, d_loss.data[0], (time.time() - iter_start_time) / opt.batchSize))
-        if total_steps % opt.save_latest_freq == 0:
-            print('saving the latest model (epoch %d, total_steps %d)' %
-                  (epoch, total_steps))
-            model.save('latest')
-
-
-total_steps = 0
+# total_steps = 0
 # adversarial training
-print('='*20 + 'Adversarial Training' + '='*20)
+print('=' * 20 + 'Adversarial Training' + '=' * 20)
 
 for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
     epoch_start_time = time.time()
@@ -203,13 +197,12 @@ for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
             trial_B = np.ones((opt.batchSize, opt.output_nc, opt.depth, opt.fineSize, opt.fineSize))
             data = {'A': trial_A, 'B': trial_B, 'A_paths': AB_path, 'B_paths': AB_path}
 
+        # data = dict(data)
 
-        #data = dict(data)
-
-        #print(data)
+        # print(data)
 
         iter_start_time = time.time()
-        #visualizer.reset()
+        # visualizer.reset()
         total_steps += opt.batchSize
         epoch_iter += opt.batchSize
         model.set_input(data)
@@ -229,26 +222,23 @@ for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
         if total_steps % opt.print_freq == 0:
             errors = model.get_current_errors()
             t = (time.time() - iter_start_time) / opt.batchSize
-            print("epoch: {}, iter: {}, G-loss: {}, time: {} seconds/batch".format(epoch,
-                i, g_mse_loss.data[0], (time.time() - iter_start_time) / opt.batchSize))
+            print("epoch: {}, iter: {}, g-mse-loss: {}, time: {} seconds/batch".format(
+                epoch, i, g_mse_loss.data[0], (time.time() - iter_start_time) / opt.batchSize))
             print(model.get_current_errors())
-            print("target seq:\n {} \ngenerated seq: {}".format(model.input_seq, model.gen_seq))
-            #visualizer.print_current_errors(epoch, epoch_iter, errors, t)
-            #if opt.display_id > 0:
-                #visualizer.plot_current_errors(epoch, float(epoch_iter)/dataset_size, opt, errors)
+            print("target seq:\n {} \ngenerated seq: {}".format(model.speedX, model.speedX_pred))
+            # visualizer.print_current_errors(epoch, epoch_iter, errors, t)
+            # if opt.display_id > 0:
+            # visualizer.plot_current_errors(epoch, float(epoch_iter)/dataset_size, opt, errors)
 
         if total_steps % opt.save_latest_freq == 0:
             print('saving the latest model (epoch %d, total_steps %d)' %
                   (epoch, total_steps))
             model.save('latest')
 
-
         if total_steps % 20010 == 0:
             print('saving the 20010 model (epoch %d, total_steps %d)' %
                   (epoch, total_steps))
             model.save(total_steps)
-
-
 
     if epoch % opt.save_epoch_freq == 0:
         print('saving the model at the end of epoch %d, iters %d' %
